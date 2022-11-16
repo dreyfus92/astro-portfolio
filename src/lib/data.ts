57505d6ccb8016d445b5de5ac4b4d9ee332a4cff
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-11-16 12:24:57 -0600

    renamed TechBadge and added a few changes to index.astro

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,31 +2,31 @@ export const technologies = [
     {
         name: 'Astro',
         imgSrc: 'https://astro.build/assets/astro-logo.svg',
-        imgAlt: 'Astro Logo',
+        altSrc: 'Astro Logo',
     },
     {
         name: 'JavaScript',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png',
-        imgAlt: 'JavaScript Logo',
+        altSrc: 'JavaScript Logo',
     },
     {
         name: 'Node.js',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png',
-        imgAlt: 'Node.js Logo',
+        altSrc: 'Node.js Logo',
     },
     {
         name: 'Python',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
-        imgAlt: 'Python Logo'
+        altSrc: 'Python Logo'
     },
     {
         name: 'React',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png',
-        imgAlt: 'React Logo',
+        altSrc: 'React Logo',
     },
     {
         name: 'TypeScript',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
-        imgAlt: 'TypeScript Logo',
+        altSrc: 'TypeScript Logo',
     },
 ]
