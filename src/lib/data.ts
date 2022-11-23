4cbf95313d0274b2e5647f62c1ee8c045ca2c794
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-11-22 21:49:27 -0600

    added links to badges and removed dead code

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,41 +3,50 @@ export const technologies = [
         name: 'Astro',
         imgSrc: 'https://bestofjs.org/logos/astro.dark.svg',
         altSrc: 'Astro Logo',
+        link: 'https://astro.build/',
     },
     {
         name: 'JavaScript',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png',
         altSrc: 'JavaScript Logo',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
     },
     {
         name: 'Node.js',
         imgSrc: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
         altSrc: 'Node.js Logo',
+        link: 'https://nodejs.org/en/',
     },
     {
         name: 'Python',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
-        altSrc: 'Python Logo'
+        altSrc: 'Python Logo',
+        link: 'https://www.python.org/',
     },
     {
         name: 'React',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png',
         altSrc: 'React Logo',
+        link: 'https://reactjs.org/',
     },
     {
         name: 'TypeScript',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
         altSrc: 'TypeScript Logo',
+        link: 'https://www.typescriptlang.org/',
+
     },
     {
         name: 'Tailwind CSS',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
         altSrc: 'Tailwind CSS Logo',
+        link: 'https://tailwindcss.com/',
     },
     {
         name: 'Next.js',
         imgSrc: 'https://static.netify.ai/logos/v/e/r/irepry/icon.png?v=1',
         altSrc: 'Next.js Logo',
+        link: 'https://nextjs.org/',
     }
 ]
 
