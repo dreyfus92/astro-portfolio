9195782e551c8765325384dfd099b00047b9fe13
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-04-21 09:36:54 -0600

    styles: added neon-green to tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'neon-green': '#87CB49',
+			}
+		},
 	},
 	plugins: [],
 	future: {
