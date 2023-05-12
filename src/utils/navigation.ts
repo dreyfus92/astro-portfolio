d6a9f29624b6cbaa82e7d3a0522d57acc91a5320
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-05-11 23:28:16 -0600

    update: refactored navbar

---
+++ src/utils/navigation.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+    {
+        route: "/",
+        text: "Home",
+    },
+    {
+        route: "/blog",
+        text: "Blog",
+    },
+    {
+        route: "/socials",
+        text: "Socials",
+    },
+];
