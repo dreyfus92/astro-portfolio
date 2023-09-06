62364d484e548bdf271497fa856473c9ce3d9d88
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-09-05 21:10:39 -0600

    feat: fully migrated images to assets folder and refactored typings

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,15 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-import readingTime from "reading-time";
+
 // Define a schema for each collection you'd like to validate.
 const postsCollection = defineCollection({
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
-        pubDate: z.date(),
         description: z.string(),
+        pubDate: z.date(),
         draft: z.boolean(),
-        image: z.object({
-            src: z.string(),
-            alt: z.string(),
-        })
+        cover: image(),
+        coverAlt: z.string(),
     })
 });
 
