9cb706c4e18780f6a74f10cfb3b092610e0a2f3d
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-09-12 23:54:44 -0600

    removed comments

--- astro.config.ts
+++ astro.config.ts
@@ -6,18 +6,11 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'dark-plus',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true
     },
     remarkPlugins: [remarkReadingTime],
